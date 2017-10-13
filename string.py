4565c89d3b0274d45c49612e2c2ddd777fb7dd10
Sanpreet Singh <32572322+sanpreet-ml@users.noreply.github.com>
2017-10-13 16:08:48 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,15 @@
+#some examples tp print string and integers together in one line
+a = "My number is";
+b = 123456789
+print(type(b))
+print(a,b)        #a is a string whereas b is int. Putting a comma between a and b can make print them
+
+######Another example#########################################################
+Manav =  "165 cm"
+pranav = "175 cm"
+print('Height of Manav is:' +str(Manav) + ", " +  'Height of Pranav is:' +str(pranav))
+
+########another example%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Harpreet =  "35 years"
+Gurpreet =  "45 years"
+print("Age of first person Harpreet is {} and Age of second person Gurpreet is {}" . format(Harpreet,Gurpreet))
